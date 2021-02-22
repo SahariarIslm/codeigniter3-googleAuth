57653285ce1749195da2b82bf0547e91e88ff087
Sahariarislm <dhruv.islam7584@gmail.com>
2021-02-22 16:58:12 +0600

    first commit

---
+++ application/config/googleplus.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$config['googleplus']['application_name'] = 'goo_auth';
+$config['googleplus']['client_id']        = '743651129031-0p5nijivas4fgqjou9au1siq9pup87kr.apps.googleusercontent.com';
+$config['googleplus']['client_secret']    = '8OzPMsn-j2czQ7q4_xQHgYGz';
+$config['googleplus']['redirect_uri']     = 'http://localhost/goo_auth/User_authentication/profile';
+$config['googleplus']['api_key']          = '';
+$config['googleplus']['scopes']           = array('profile','email');
+
